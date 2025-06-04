cb4be7b4d213c116b34f01aa81e898490fb53fcb
andre2dev <andredev@duck.com>
2025-06-04 02:53:31 +0000

    chore sqlite local test updated to postgres

--- api/app/Models/__init__.py
+++ api/app/Models/__init__.py
@@ -14,5 +14,5 @@ db_pass = os.environ['POSTGRES_PASSWORD']
 db_name = os.environ['POSTGRES_DB']
 host = os.environ['HOST']
 
-# DATABASE_URL = f'postgresql://{db_user}:{db_pass}@{host}:5432/{db_name}'
-DATABASE_URL = f'sqlite:///data.db'
+DATABASE_URL = f'postgresql://{db_user}:{db_pass}@{host}:5432/{db_name}'
+# DATABASE_URL = f'sqlite:///data.db'
